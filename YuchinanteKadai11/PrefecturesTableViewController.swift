ff6116fbf118270331364b85fd75b59630c02f40
yuya-3110 <ysuu45143745@gmail.com>
2024-04-04 01:20:50 +0900

    second commit

--- YuchinanteKadai11/PrefecturesTableViewController.swift
+++ YuchinanteKadai11/PrefecturesTableViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class PrefecturesTableViewController: UIViewController {
+// 都道府県選択画面からの通知を受け取るためのデリゲートプロトコル
+protocol PrefecturesViewControllerDelegate: AnyObject {
+    // 選択された都道府県名を通知するメソッド
+    func didSelectPrefectures(_ prefecture: String)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+// 都道府県一覧を表示するテーブルビューの管理を行うクラス
+class PrefecturesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-        // Do any additional setup after loading the view.
+    // デリゲートプロトコルへの参照を弱参照で保持する
+    weak var delegate: PrefecturesViewControllerDelegate?
+
+    // 表示する都道府県名のリスト
+    private let prefecturesList = [
+        "北海道", "青森県", "岩手県", "宮城県", "秋田県",
+        "山形県", "福島県", "茨城県", "栃木県", "群馬県",
+        "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
+        "富山県", "石川県", "福井県", "山梨県", "長野県",
+        "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県",
+        "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+        "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+        "徳島県", "香川県", "愛媛県", "高知県", "福岡県",
+        "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県",
+        "鹿児島県", "沖縄県"
+    ]
+
+    // セクション内の行数を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return prefecturesList.count
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // セルの内容を設定するメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能なセルを取得して、都道府県名を表示する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = prefecturesList[indexPath.row]
+        return cell
     }
-    */
 
+    // テーブルビューの行が選択された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 選択された都道府県名を取得し、デリゲートに通知して画面を閉じる
+        let selectedPrefecture = prefecturesList[indexPath.row]
+        delegate?.didSelectPrefectures(selectedPrefecture)
+        dismiss(animated: true, completion: nil)
+    }
 }
--- YuchinanteKadai11/ViewController.swift
+++ YuchinanteKadai11/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// PrefecturesViewControllerDelegateプロトコルを適用するViewControllerクラスを定義
+class ViewController: UIViewController, PrefecturesViewControllerDelegate {
+    // 都道府県名を表示するUILabelのインスタンスを宣言
+    @IBOutlet weak var prefectureLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // ユーザーがボタンをタップしたときに呼ばれるメソッド
+    @IBAction func didTapSelectButton(_ sender: Any) {
+        // Segueを実行し、"toPrefectureTableViewController"という識別子を持つ遷移を行う
+        performSegue(withIdentifier: "toPrefectureTableViewController", sender: self)
     }
 
+    // Segueが実行される直前に呼ばれるメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Segueの識別子が"toPrefectureTableViewController"である場合の処理
+        if segue.identifier == "toPrefectureTableViewController" {
+            // 遷移先がUINavigationControllerであることを確認し、その中のPrefecturesTableViewControllerにdelegateを設定する
+            if let navigationController = segue.destination as? UINavigationController, let prefecturesTableViewController = navigationController.topViewController as? PrefecturesTableViewController {
+                prefecturesTableViewController.delegate = self
+            }
+        }
+    }
 
+    // PrefecturesViewControllerDelegateプロトコルで定義されたメソッド。都道府県が選択された時に呼ばれ、都道府県名をUILabelに表示する
+    func didSelectPrefectures(_ prefecture: String) {
+        prefectureLabel.text = prefecture
+    }
+
+    // 戻るボタンがタップされたときに呼ばれるメソッド
+    @IBAction func didTapBackButton(segue:UIStoryboardSegue){
+        }
 }
 
